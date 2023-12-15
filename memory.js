86383f862c9497fc6ba541c3bde13903147a0559
Davide Baldi <d.baldi88@gmail.com>
2023-12-15 09:52:17 +0100

    shuffle function fixed

--- memory.js
+++ memory.js
@@ -1,4 +1,4 @@
-const emojis= [
+const EMOJIS= [
     String.fromCodePoint(0x1F621),
     String.fromCodePoint(0x1F621),
     String.fromCodePoint(0x1F922),
@@ -17,6 +17,17 @@ const emojis= [
     String.fromCodePoint(0x1F977)
 ];
 
+let copy_EMOJIS = Array.from(EMOJIS);
+let placeholderShuffledEmojis = new Array(16);
 
+const shuffleCards = () => {
+  for (i=0; i<EMOJIS.length; i++) {
+    removeEmojiIndex = Math.floor(Math.random() * copy_EMOJIS.length);
+    placeholderShuffledEmojis[i] = copy_EMOJIS.splice(removeEmojiIndex, 1)[0];
+    
+  }
+}
+shuffleCards();
+console.log(placeholderShuffledEmojis);
 
 
