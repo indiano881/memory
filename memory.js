e71a1815ef6a648cec75911046171f07e015595a
Davide Baldi <d.baldi88@gmail.com>
2023-12-15 09:42:26 +0100

    emojis fixed

---
+++ memory.js
@@ -0,0 +1,22 @@
+const emojis= [
+    String.fromCodePoint(0x1F621),
+    String.fromCodePoint(0x1F621),
+    String.fromCodePoint(0x1F922),
+    String.fromCodePoint(0x1F922),
+    String.fromCodePoint(0x1F976),
+    String.fromCodePoint(0x1F976),
+    String.fromCodePoint(0x1F608),
+    String.fromCodePoint(0x1F608),
+    String.fromCodePoint(0x1F480),
+    String.fromCodePoint(0x1F480),
+    String.fromCodePoint(0x1F47D),
+    String.fromCodePoint(0x1F47D),
+    String.fromCodePoint(0x1F63B),
+    String.fromCodePoint(0x1F63B),
+    String.fromCodePoint(0x1F977),
+    String.fromCodePoint(0x1F977)
+];
+document.querySelector("h1").innerHTML= emojis.join(" ");
+
+
+
